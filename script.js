f957ed561f80013579d6a3d0553e5dd5f579ee77
Karen Ying <karenying7@gmail.com>
2020-09-22 21:37:09 -0400

    Inital commit

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var Clock = (function () {
+    var exports = function (element) {
+      this._element = element;
+      var html = "";
+      for (var i = 0; i < 6; i++) {
+        html += "<span>&nbsp;</span>";
+      }
+      this._element.innerHTML = html;
+      this._slots = this._element.getElementsByTagName("span");
+      this._tick();
+    };
+    exports.prototype = {
+      _tick: function () {
+        var time = new Date();
+        this._update(
+          this._pad(time.getHours()) +
+            this._pad(time.getMinutes()) +
+            this._pad(time.getSeconds())
+        );
+        var self = this;
+        setTimeout(function () {
+          self._tick();
+        }, 1000);
+      },
+      _pad: function (value) {
+        return ("0" + value).slice(-2);
+      },
+      _update: function (timeString) {
+        var i = 0,
+          l = this._slots.length,
+          value,
+          slot,
+          now;
+        for (; i < l; i++) {
+          value = timeString.charAt(i);
+          slot = this._slots[i];
+          now = slot.dataset.now;
+          if (!now) {
+            slot.dataset.now = value;
+            slot.dataset.old = value;
+            continue;
+          }
+          if (now !== value) {
+            this._flip(slot, value);
+          }
+        }
+      },
+      _flip: function (slot, value) {
+        slot.classList.remove("flip");
+        slot.dataset.old = slot.dataset.now;
+        slot.dataset.now = value;
+        slot.offsetLeft;
+        slot.classList.add("flip");
+      },
+    };
+    return exports;
+  })();
+  var i = 0,
+    clocks = document.querySelectorAll(".clock"),
+    l = clocks.length;
+  for (; i < l; i++) {
+    new Clock(clocks[i]);
+  }
+});
